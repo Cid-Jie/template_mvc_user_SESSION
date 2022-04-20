21229b1b3a3cef5164da8c088e35f09ee9eaf22d
Cid-Jie <pcidjie@gmail.com>
2022-04-20 20:56:43 +0200

    add files

---
+++ Controller/AbstractController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\UserManager;
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
+
+abstract class AbstractController
+{
+    protected Environment $twig;
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        $loader = new FilesystemLoader(APP_VIEW_PATH);
+        $this->twig = new Environment(
+            $loader,
+            [
+                'cache' => false,
+                'debug' => (ENV === 'dev'),
+            ]
+        );
+        $this->twig->addExtension(new DebugExtension());
+        $userManager = new UserManager();
+        $this->user = isset($_SESSION['user_id']) ? $userManager->selectOneById($_SESSION['user_id']) : false;
+        $this->twig->addGlobal('user', $this->user);
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\UserManager;
+
+class UserController extends AbstractController
+{
+    public function login(): string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $control = array_map('trim', $_POST);
+
+            $userManager = new UserManager();
+            $user = $userManager->selectOneByEmail($control['email']);
+
+            if($user && password_verify($control['password'], $user['password']))#
+            $_SESSION['user_id'] = $user['id'];
+            header('Location: /');
+            exit();
+        }    
+        return $this->twig->render('User/login.html.twig');
+    }
+
+    public function logout()
+    {
+        session_destroy();
+        header('Location: /');
+        exit();
+    }
+
+    public function register(): string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //      @todo make some controls and if errors send them to the view
+                    $credentials = $_POST;
+                    $userManager = new UserManager();
+                    if ($userManager->insert($credentials)) {
+                        return $this->login();
+                    }
+        }
+        return $this->twig->render('User/register.html.twig');
+    }
+}
